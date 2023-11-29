0601b52a98440e29462e369dd5491cb08120cac9
NEX-DoT <nex.dot2@gmail.com>
2023-11-30 01:16:18 +0330

    the end

--- components/models/tabelsPage/TabelNew.js
+++ components/models/tabelsPage/TabelNew.js
@@ -1,10 +1,31 @@
-
+import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell} from "@nextui-org/react";import Card from "./Card";
 const TabelNew = ({data}) => {
+    const standings = data[0].league.standings[0];
+    console.log(standings);
     return (
-        <div>
-            {console.log(data)}
+         <div className=" w-3/4 m-auto">
+            <Table aria-label="Example static collection table">
+      <TableHeader>
+        <TableColumn>Rank</TableColumn>
+        <TableColumn>Team</TableColumn>
+        <TableColumn>PTS</TableColumn>
+        <TableColumn>W</TableColumn>
+        <TableColumn>D</TableColumn>
+        <TableColumn>L</TableColumn>
+        <TableColumn>Form</TableColumn>
+        <TableColumn>GD</TableColumn>
+      </TableHeader>
+      <TableBody items={standings}>
+        {(items)=>(
+            <TableRow key={items.rank}>
+
+            </TableRow>
+        )}
+      </TableBody>
+    </Table>
         </div>
     );
 };
 
 export default TabelNew;
+
