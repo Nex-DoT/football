3092262ef9a1d0ad643c0974258ffa426f4a7566
NEX-DoT <nex.dot2@gmail.com>
2024-02-15 19:19:38 +0330

    menu complited

--- components/models/dashboard/Menu.js
+++ components/models/dashboard/Menu.js
@@ -3,8 +3,15 @@ import { FaRegUser } from "react-icons/fa";
 import { AiOutlineHeart } from "react-icons/ai";
 import { AiOutlineBell } from "react-icons/ai";
 import { BiLogOut } from "react-icons/bi";
-
+import { useRouter } from 'next/router';
 const Menu = ({active , setActive , onClickHandeler}) => {
+    const router = useRouter()
+    const logoutHandler =async()=>{
+        const res = await fetch('/api/auth/logout');
+        const data = await res.json();
+        console.log(data);
+        router.push('myAccount')
+    }
     return (
         <div className=' w-40 rounded-e-md h-210 mt-1 mb-1 flex flex-col items-start justify-between pb-8 pt-2 absolute '> 
         <div className=' h-48 flex flex-col justify-around font-Rubic text-sm'>
@@ -19,7 +26,7 @@ const Menu = ({active , setActive , onClickHandeler}) => {
             </button>
         </div>
 
-        <button type='button' name='User' className={`flex items-center justify-center cursor-pointer w-16 h-14 bg-color4 sm:bg-white  rounded-e-md transition-all `} >
+        <button type='button' onClick={logoutHandler} name='User' className={`flex items-center justify-center cursor-pointer w-16 h-14 bg-color4 sm:bg-white  rounded-e-md transition-all `} >
                 <BiLogOut className='w-1/3 h-7 flex items-center'/>
          </button>
 
